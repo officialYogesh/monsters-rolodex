91d90d1207e3e51845e491c2fe9efc914033f33d
Yogesh Patil <officialyogeshp@gmail.com>
2021-10-22 18:17:34 +0530

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -8,19 +9,40 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
+
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
+      .then((response) => {
+        response[2].name = "Aparna Sarawadekar";
+        response[2].email = "Aparna@yesenia.net";
+        return response;
+      })
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="Search Monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
